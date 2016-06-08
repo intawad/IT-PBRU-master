e92e1c58a5c361aa00be2886c424293c7d1c7915
intawad <intawad.d@gmail.com>
2016-06-08 11:02:50 +0700

    Authen finish

---
+++ app/src/main/java/appewtc/masterung/itpbru/MyManage.java
@@ -0,0 +1,47 @@
+package appewtc.masterung.itpbru;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by masterUNG on 6/7/16 AD.
+ */
+public class MyManage {
+
+    //Explicit
+    private MyOpenHelper myOpenHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_name = "Name";
+    public static final String column_surname = "Surname";
+    public static final String column_user = "User";
+    public static final String column_password = "Password";
+
+    public MyManage(Context context) {
+
+        myOpenHelper = new MyOpenHelper(context);
+        sqLiteDatabase = myOpenHelper.getWritableDatabase();
+
+    }   // Constructor
+
+    public long addNewUser(String strID,
+                           String strName,
+                           String strSurname,
+                           String strUser,
+                           String strPassword) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_id, strID);
+        contentValues.put(column_name, strName);
+        contentValues.put(column_surname, strSurname);
+        contentValues.put(column_user, strUser);
+        contentValues.put(column_password, strPassword);
+
+        return sqLiteDatabase.insert(user_table, null, contentValues);
+    }
+
+
+}   // Main Class
